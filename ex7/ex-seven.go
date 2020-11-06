c45f8396cb9725407a83868340fb1079dd607fe4
Niles87 <nbing19@gmail.com>
2020-11-06 06:04:51 -0800

    exercise 7

---
+++ ex7/ex-seven.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// use comparison operators and print results
+func main() {
+	a := 1 == 4
+	b := 987 <= 1000
+	c := 390 >= 444
+	d := 5 != 5
+	e := 5 < 9
+	f := 8 > 100
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+}
