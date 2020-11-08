0f8372c26e7c765db5f8f54ed88f6f09f44ff1f2
Niles87 <nbing19@gmail.com>
2020-11-07 19:25:12 -0800

    exercise 3 section 7

---
+++ ex9/ex-nine.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// create variable then print its decimal binary and hex
+// shift the bits to the left once and assign to new variable then print the same as above
+func main() {
+	x := 42
+	fmt.Printf("%d\t%b\t%#x\n", x, x, x)
+	y := (x << 1)
+	fmt.Printf("%d\t%b\t%#x\n", y, y, y)
+
+}
