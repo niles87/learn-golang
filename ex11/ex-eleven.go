a47328b616ab823c255278225e6ff9a8743b1c7f
Niles87 <nbing19@gmail.com>
2020-11-07 19:25:38 -0800

    exercise 5 section 7

---
+++ ex11/ex-eleven.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// using iota create 4 constants for the last 4 years then print the values
+const (
+	pastYears = 2016
+	four      = pastYears + iota
+	three     = pastYears + iota
+	two       = pastYears + iota
+	one       = pastYears + iota
+)
+
+func main() {
+	fmt.Println(four, three, two, one)
+}
