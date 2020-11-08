b9021febc6b6a5046426733374a3f020fe92a429
Niles87 <nbing19@gmail.com>
2020-11-08 11:46:16 -0800

    exercise 19

---
+++ ex19/ex-nineteen.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// switch statement with no switch expression
+func main() {
+	s := 10
+	switch {
+	case s == 44:
+		fmt.Println(s)
+	case s == 10:
+		fmt.Println(s)
+	}
+}
