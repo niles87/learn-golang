7f4027906254bf0b620394af5ee7b7a81cdafddc
Niles87 <nbing19@gmail.com>
2020-11-08 11:45:55 -0800

    exercise 17

---
+++ ex17/ex-seventeen.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// create if statement
+func main() {
+	x := 7
+	if x > 8 {
+		fmt.Println("x is greater than 8")
+	} else {
+		fmt.Println("x is less than or equal to 8")
+	}
+}
