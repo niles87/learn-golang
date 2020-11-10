bd8b26798a769789452fdffc4d5cf7e9cefe9882
Niles87 <nbing19@gmail.com>
2020-11-10 06:00:12 -0800

    exercise 21

---
+++ ex21/ex-twentyone.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// create a composite literal array with a length of 5 and assign values for each position
+// range over array and print values then print the type of array
+func main() {
+	var arr = [5]int{1, 2, 3, 4, 5}
+	for i, v := range arr {
+		fmt.Println(i, v)
+	}
+	fmt.Printf("%T\n", arr)
+}
